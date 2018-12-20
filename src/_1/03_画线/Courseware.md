d8db5586d26ab033156e5f2af1bd0f660a68400c
wangrongzhan <rongzhan.wang@lejent.com>
2018-12-20 18:17:56 +0800

    修改

--- src/_1/03_画线/Courseware.md
+++ src/_1/03_画线/Courseware.md
@@ -1,4 +1,29 @@
 ﻿# 02_画线
 
+http://doc.qt.io/qt-5/qt.html#GlobalColor-enum有些全局的变量如
+
+```
+Qt::ImhHiddenText
+Qt::Key_Escape
+Qt::white
+```
+
+在kitech\qt.go\qtcore\qnamespace.go
+
+Qt Namespace 中定义了很多的全局变量值,咱们可以在qnamespace.go 找到对应的名称
+
+如上例子中int 7,就可以Qt__red来代替.
+
+```
+		f1 := func(painter *qtgui.QPainter) {
+			var qcolor qtgui.QColor
+
+			s := qcolor.NewForInherit1(qtcore.Qt__red)
+
+			painter.SetPen(s)
+			painter.DrawLine2(10, 10, 100, 100)
+		}
+		mw.Form.InheritInitPainter(f1)
+```
 ## Pen Style
 
--- src/_1/03_画线/main.go
+++ src/_1/03_画线/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"os"
 
+	"github.com/kitech/qt.go/qtcore"
+
 	"github.com/kitech/qt.go/qtgui"
 	"github.com/kitech/qt.go/qtwidgets"
 )
@@ -28,75 +30,14 @@ func main() {
 	if eg {
 		f1 := func(painter *qtgui.QPainter) {
 			var qcolor qtgui.QColor
-			//s := qcolor.NewForInherit()// 默认颜色
-			s := qcolor.NewForInherit1(qcolor.Blue())
+
+			s := qcolor.NewForInherit1(qtcore.Qt__red)
+
 			painter.SetPen(s)
 			painter.DrawLine2(10, 10, 100, 100)
 		}
-
-		mw.Form.InheritInitPainter(f1)
-		//mw.Form.InheritInitPainter(f1)// 不能同时执行多次
-	}
-
-	eg = false
-	if eg {
-		// 多线颜色
-		f1 := func(painter *qtgui.QPainter) {
-			var qcolor qtgui.QColor
-			for i := 0; i <= 30; i++ {
-				s := qcolor.NewForInherit1(i)
-				painter.SetPen(s)
-				painter.DrawLine2(10, 10+i*10, 100, 10+i*10)
-			}
-		}
 		mw.Form.InheritInitPainter(f1)
 	}
 
-	eg = false
-	if eg {
-		// 多线颜色 NewForInherit2 rgba
-		f1 := func(painter *qtgui.QPainter) {
-			var qcolor qtgui.QColor
-
-			// [-2] void QColor(int, int, int, int)
-			// 查帮助文档 QColor::QColor(int r, int g, int b, int a = ...)
-			// QColor::QColor ( int r, int g, int b, int a = 255 )
-			// func (*QColor) NewForInherit2(r int, g int, b int, a int) *QColor
-			s := qcolor.NewForInherit2(255, 255, 255, 255)
-			painter.SetPen(s)
-			painter.DrawLine2(10, 10, 100, 10)
-			s = qcolor.NewForInherit2(255, 0, 0, 255)
-			painter.SetPen(s)
-			painter.DrawLine2(10, 40, 100, 40)
-			s = qcolor.NewForInherit2(0, 255, 0, 255)
-			painter.SetPen(s)
-			painter.DrawLine2(10, 70, 100, 70)
-			s = qcolor.NewForInherit2(0, 0, 255, 255)
-			painter.SetPen(s)
-			painter.DrawLine2(10, 100, 100, 100)
-		}
-		mw.Form.InheritInitPainter(f1)
-	}
-
-	eg = false
-	if eg {
-		// 多线颜色 NewForInherit2p rgb
-		f1 := func(painter *qtgui.QPainter) {
-			var qcolor qtgui.QColor
-			s := qcolor.NewForInherit2p(255, 255, 255)
-			painter.SetPen(s)
-			painter.DrawLine2(10, 10, 100, 10)
-			s = qcolor.NewForInherit2p(255, 0, 0)
-			painter.SetPen(s)
-			painter.DrawLine2(10, 40, 100, 40)
-			s = qcolor.NewForInherit2p(0, 255, 0)
-			painter.SetPen(s)
-			painter.DrawLine2(10, 70, 100, 70)
-			s = qcolor.NewForInherit2p(0, 0, 255)
-			painter.SetPen(s)
-			painter.DrawLine2(10, 100, 100, 100)
-		}
-		mw.Form.InheritInitPainter(f1)
-	}
 	app.Exec()
 }
