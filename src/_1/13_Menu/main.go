85a76be862c3c36fcaa8178c07966e065a82e51e
wangrongzhan <rongzhan.wang@lejent.com>
2018-12-25 11:17:01 +0800

    qtoolbutton

--- src/_1/13_Menu/main.go
+++ src/_1/13_Menu/main.go
@@ -15,34 +15,29 @@ func main() {
 	app := qtwidgets.NewQApplication(len(os.Args), os.Args, 0)
 
 	mw := NewUi_Form2()
-
-	// 不成功....
-	btn := qtwidgets.NewQPushButton(mw.Form)
-	btn.SetText("主菜单")
-	btn.Move(50, 50)
-
-	qmenu := qtwidgets.NewQMenu(nil)
-	qmenu.AddSection("设置")
-	qmenu.AddSection("版本检测")
-	qmenu.AddSeparator()
-	qmenu.AddSection("关于我们")
-	qmenu.AddSection("退出")
-	qmenu.Move(100, 100)
-
-	btn.SetMenu(qmenu)
-
 	if true {
 		// 加载qss
 		qss := qtcore.NewQFile1("./main.qss")
 		ret := qss.Open(qtcore.QIODevice__ReadOnly)
 		if ret {
 			str := qtcore.QString_FromUtf81(qss.ReadAll())
-			qss.Close()
-
 			mw.Form.SetStyleSheet(str)
-
+			qss.Close()
 		}
 	}
+
+	qmenu := qtwidgets.NewQMenu(mw.Form)
+	qmenu.AddAction("设置")
+	qmenu.AddAction("版本检测")
+	qmenu.AddSeparator()
+	qmenu.AddAction("关于我们")
+	qmenu.AddAction("退出")
+
+	btn := qtwidgets.NewQPushButton(mw.Form)
+	btn.SetText("主菜单")
+	btn.SetFixedSize1(100, 30)
+	btn.Move(50, 50)
+	btn.SetMenu(qmenu)
 
 	mw.Form.Show()
 	app.Exec()
---
+++ src/_1/14_QToolButton/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	_ "fmt"
+	"os"
+
+	"github.com/kitech/qt.go/qtgui"
+
+	_ "github.com/kitech/qt.go/qtgui"
+
+	"github.com/kitech/qt.go/qtcore"
+	_ "github.com/kitech/qt.go/qtrt"
+	"github.com/kitech/qt.go/qtwidgets"
+)
+
+func main() {
+	app := qtwidgets.NewQApplication(len(os.Args), os.Args, 0)
+
+	mw := NewUi_Form2()
+	if true {
+		// 加载qss
+		qss := qtcore.NewQFile1("./main.qss")
+		ret := qss.Open(qtcore.QIODevice__ReadOnly)
+		if ret {
+			str := qtcore.QString_FromUtf81(qss.ReadAll())
+			mw.Form.SetStyleSheet(str)
+			qss.Close()
+		}
+	}
+	if false {
+		// 工具栏
+		qtoolbtn := qtwidgets.NewQToolButton(mw.Form)
+		//qtoolbtn.SetArrowType(qtcore.Qt__DownArrow)
+		qtoolbtn.SetArrowType(qtcore.Qt__LeftArrow)
+
+		qtoolbtn.SetText("left arrow")
+		qtoolbtn.SetToolButtonStyle(qtcore.Qt__ToolButtonTextUnderIcon)
+		//qtoolbtn.SetStyleSheet("QToolButton{border: none; background: rgb(68, 69, 73); color: rgb(0, 160, 230);}")
+
+	}
+
+	if true {
+		qAction := qtwidgets.NewQAction(mw.Form)
+		qAction.SetText("工具按钮")
+		icon := qtgui.NewQIcon2(":/images/logo1.png")
+		qAction.SetIcon(icon)
+		qtoolbtn := qtwidgets.NewQToolButton(mw.Form)
+		qtoolbtn.SetIconSize(qtcore.NewQSize1(48, 48))
+		//		qAction.SetToolTip("战斗不止")// 不生效?
+		qtoolbtn.SetDefaultAction(qAction)
+		qtoolbtn.SetToolButtonStyle(qtcore.Qt__ToolButtonTextUnderIcon)
+		qtoolbtn.Move(100, 100)
+
+		qtoolbtn.SetFixedSize1(220, 100)
+	}
+
+	mw.Form.Show()
+	app.Exec()
+}
